7a4f64f87da665e7295785baed8a8e0ec3926d1d
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-10-25 23:13:28 +0200

    Damage type thing

--- src/main/java/team/lodestar/lodestone/handlers/LodestoneAttributeEventHandler.java
+++ src/main/java/team/lodestar/lodestone/handlers/LodestoneAttributeEventHandler.java
@@ -1,18 +1,14 @@
 package team.lodestar.lodestone.handlers;
 
 import net.minecraft.core.*;
-import net.minecraft.core.registries.*;
-import net.minecraft.resources.*;
 import net.minecraft.world.damagesource.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.level.*;
+import net.minecraft.world.entity.player.*;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import team.lodestar.lodestone.helpers.*;
 import team.lodestar.lodestone.registry.common.LodestoneAttributeRegistry;
 import team.lodestar.lodestone.registry.common.tag.*;
-
-import javax.annotation.*;
 
 /**
  * A handler for common attributes I use in my mods.
@@ -26,27 +22,32 @@ public class LodestoneAttributeEventHandler {
         var target = event.getEntity();
 
         float amount = event.getAmount();
-        if (source.typeHolder().is(LodestoneDamageTypeTags.IS_MAGIC)) {
+        var damageType = source.typeHolder();
+        if (damageType.is(LodestoneDamageTypeTags.IS_MAGIC)) {
             var magicResistance = target.getAttribute(LodestoneAttributeRegistry.MAGIC_RESISTANCE.get());
             if (magicResistance != null) {
                 amount /= (float) Math.max(magicResistance.getValue(), 0.01f);
             }
         }
         if (source.getEntity() instanceof LivingEntity attacker) {
-            if (source.typeHolder().is(LodestoneDamageTypeTags.IS_MAGIC)) {
+            if (damageType.is(LodestoneDamageTypeTags.IS_MAGIC)) {
                 var magicProficiency = attacker.getAttribute(LodestoneAttributeRegistry.MAGIC_PROFICIENCY.get());
                 if (magicProficiency != null) {
                     amount *= (float) magicProficiency.getValue();
                 }
                 event.setAmount(amount);
             }
-            else if (source.typeHolder().is(LodestoneDamageTypeTags.CAN_TRIGGER_MAGIC)) {
+            else if (damageType.is(LodestoneDamageTypeTags.CAN_TRIGGER_MAGIC)) {
                 AttributeInstance magicDamage = attacker.getAttribute(LodestoneAttributeRegistry.MAGIC_DAMAGE.get());
                 if (magicDamage != null) {
                     if (magicDamage.getValue() > 0 && !target.isDeadOrDying()) {
+                        float magicDamageAmount = (float) magicDamage.getValue();
+                        if (attacker instanceof Player player && damageType.is(LodestoneDamageTypeTags.IGNORES_MAGIC_ATTACK_COOLDOWN_SCALAR)) {
+                            magicDamageAmount *= player.getAttackStrengthScale(0);
+                        }
                         var magic = DamageTypeHelper.create(DamageTypes.MAGIC, attacker);
                         target.invulnerableTime = 0;
-                        target.hurt(magic, (float) magicDamage.getValue());
+                        target.hurt(magic, magicDamageAmount);
                     }
                 }
             }
--- src/main/java/team/lodestar/lodestone/registry/common/tag/LodestoneDamageTypeTags.java
+++ src/main/java/team/lodestar/lodestone/registry/common/tag/LodestoneDamageTypeTags.java
@@ -9,6 +9,7 @@ public class LodestoneDamageTypeTags {
 
     public static final TagKey<DamageType> IS_MAGIC = forgeTag("is_magic");
     public static final TagKey<DamageType> CAN_TRIGGER_MAGIC = forgeTag("can_trigger_magic_damage");
+    public static final TagKey<DamageType> IGNORES_MAGIC_ATTACK_COOLDOWN_SCALAR = forgeTag("ignores_magic_attack_cooldown_scalar");
 
     public static TagKey<DamageType> forgeTag(String path) {
         return TagKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("forge", path));
