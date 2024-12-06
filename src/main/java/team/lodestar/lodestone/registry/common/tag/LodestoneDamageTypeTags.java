72bc5b726f2b5ead12fd791a3d70b808bf022049
mrsterner <simon@sterner.dev>
2024-12-06 10:39:43 +0100

    ddd

--- src/main/java/team/lodestar/lodestone/registry/common/tag/LodestoneDamageTypeTags.java
+++ src/main/java/team/lodestar/lodestone/registry/common/tag/LodestoneDamageTypeTags.java
@@ -7,11 +7,11 @@ import net.minecraft.world.damagesource.*;
 
 public class LodestoneDamageTypeTags {
 
-    public static final TagKey<DamageType> IS_MAGIC = forgeTag("is_magic");
-    public static final TagKey<DamageType> CAN_TRIGGER_MAGIC = forgeTag("can_trigger_magic_damage");
-    public static final TagKey<DamageType> IGNORES_MAGIC_ATTACK_COOLDOWN_SCALAR = forgeTag("ignores_magic_attack_cooldown_scalar");
+    public static final TagKey<DamageType> IS_MAGIC = cTag("is_magic");
+    public static final TagKey<DamageType> CAN_TRIGGER_MAGIC = cTag("can_trigger_magic_damage");
+    public static final TagKey<DamageType> IGNORES_MAGIC_ATTACK_COOLDOWN_SCALAR = cTag("ignores_magic_attack_cooldown_scalar");
 
-    public static TagKey<DamageType> forgeTag(String path) {
-        return TagKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath("forge", path));
+    public static TagKey<DamageType> cTag(String path) {
+        return TagKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath("c", path));
     }
 }
