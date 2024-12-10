c716d981fd5a113abd0675cd99913ede362eee6c
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-12-10 14:09:34 +0100

    Update LodestoneBlockTagsProvider.java

--- src/main/java/team/lodestar/lodestone/systems/datagen/providers/LodestoneBlockTagsProvider.java
+++ src/main/java/team/lodestar/lodestone/systems/datagen/providers/LodestoneBlockTagsProvider.java
@@ -6,12 +6,14 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.*;
 import org.jetbrains.annotations.Nullable;
 import team.lodestar.lodestone.systems.block.LodestoneBlockProperties;
 import team.lodestar.lodestone.systems.datagen.LodestoneDatagenBlockData;
 
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.*;
 
 public abstract class LodestoneBlockTagsProvider extends BlockTagsProvider {
 
@@ -19,8 +21,9 @@ public abstract class LodestoneBlockTagsProvider extends BlockTagsProvider {
         super(output, lookupProvider, modId, existingFileHelper);
     }
 
-    public void addTagsFromBlockProperties(Collection<Block> blocks) {
-        for (Block block : blocks) {
+    public void addTagsFromBlockProperties(Set<DeferredHolder<Block, ? extends Block>> blocks) {
+        var blockList = blocks.stream().map(DeferredHolder::get).sorted().toList();
+        for (Block block : blockList) {
             LodestoneBlockProperties properties = (LodestoneBlockProperties) block.properties();
             LodestoneDatagenBlockData data = properties.getDatagenData();
             for (TagKey<Block> tag : data.getTags()) {
