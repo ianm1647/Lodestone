c69a252ba18e23c71b925d886d1db83d4ba22bd0
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-12-17 09:17:40 +0100

    make the tab build event actually run

--- src/main/java/team/lodestar/lodestone/events/SetupEvents.java
+++ src/main/java/team/lodestar/lodestone/events/SetupEvents.java
@@ -5,8 +5,10 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
+import net.neoforged.neoforge.event.*;
 import team.lodestar.lodestone.handlers.ThrowawayBlockDataHandler;
 import team.lodestar.lodestone.registry.common.LodestoneCommandArgumentTypes;
+import team.lodestar.lodestone.systems.item.*;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class SetupEvents {
@@ -19,5 +21,10 @@ public class SetupEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void lateSetup(InterModEnqueueEvent event) {
         ThrowawayBlockDataHandler.wipeCache(event);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void buildCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        LodestoneItemProperties.populateItemGroups(event);
     }
 }
