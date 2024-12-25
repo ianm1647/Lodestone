c28a9f8e5a3d03234e38ee40b2fcf6fab3c0fff0
SammySemicolon <malummoddevelopment@gmail.com>
2024-12-25 17:21:30 +0100

    Revert "Actually fix NBTHelper stuff"
    This reverts commit 21ba0252914759096fa4b8356616751e5e41c8a9.

--- src/main/java/team/lodestar/lodestone/helpers/NBTHelper.java
+++ src/main/java/team/lodestar/lodestone/helpers/NBTHelper.java
@@ -5,17 +5,12 @@ import net.minecraft.nbt.*;
 import net.minecraft.world.level.block.Block;
 
 import java.util.*;
-import java.util.function.Function;
 
-import static net.minecraft.nbt.Tag.TAG_INT;
 import static team.lodestar.lodestone.LodestoneLib.LOGGER;
 
 public class NBTHelper {
 
-    public static BlockPos readBlockPos(CompoundTag tag, String listTagName) {
-        return readBlockPos(tag.getList(listTagName, TAG_INT));
-    }
-    public static BlockPos readBlockPos(ListTag tag) {
+    public static BlockPos readBlockPos(Tag tag) {
         return BlockPos.CODEC.parse(NbtOps.INSTANCE, tag).resultOrPartial(LOGGER::error).orElse(null);
     }
 
