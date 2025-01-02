a842df60a6c779f566b0ff3ec886c9369f9a921f
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2025-01-01 20:24:46 -0800

    sampleVolumeAtlasInterpolated

--- src/main/resources/assets/lodestone/shaders/include/sampling.glsl
+++ src/main/resources/assets/lodestone/shaders/include/sampling.glsl
@@ -1,9 +1,9 @@
 /*
     * Samples a 2D texture atlas which represents a sliced 3D volume.
 */
-vec4 sampleVolumeAtlas(in sampler2D volume, in ivec2 spriteDims, in vec3 worldPos) {
+vec4 sampleVolumeAtlas(in sampler2D volume, in ivec2 spriteDims, in vec3 texCoord3D) {
     float numSlices   = float(spriteDims.x * spriteDims.y);
-    float sliceFloat  = clamp(worldPos.z, 0.0, 1.0 - 1e-6) * numSlices;
+    float sliceFloat  = clamp(texCoord3D.z, 0.0, 1.0 - 1e-6) * numSlices;
     int   sliceIndex  = int(floor(sliceFloat));
 
     int row = sliceIndex / spriteDims.x;
@@ -12,8 +12,8 @@ vec4 sampleVolumeAtlas(in sampler2D volume, in ivec2 spriteDims, in vec3 worldPo
     float tileSizeX = 1.0 / float(spriteDims.x);
     float tileSizeY = 1.0 / float(spriteDims.y);
 
-    float u = (float(col) + worldPos.x) * tileSizeX;
-    float v = (float(row) + worldPos.y) * tileSizeY;
+    float u = (float(col) + texCoord3D.x) * tileSizeX;
+    float v = (float(row) + texCoord3D.y) * tileSizeY;
 
     return texture(volume, vec2(u, v));
 }
@@ -23,9 +23,9 @@ vec4 sampleVolumeAtlas(in sampler2D volume, in ivec2 spriteDims, in vec3 worldPo
     * The sample is interpolated between the two nearest slices.
     * Uses twice as many texture lookups as sampleVolumeAtlas.
 */
-vec4 sampleVolumeAtlasInterpolated(in sampler2D volume, in ivec2 spriteDims, in vec3 worldPos) {
+vec4 sampleVolumeAtlasInterpolated(in sampler2D volume, in ivec2 spriteDims, in vec3 texCoord3D) {
     float numSlices  = float(spriteDims.x * spriteDims.y);
-    float sliceFloat = worldPos.z * (numSlices - 1.0);
+    float sliceFloat = texCoord3D.z * (numSlices - 1.0);
     int   sliceIndex = int(floor(sliceFloat));
     int   sliceIndex2 = min(sliceIndex + 1, spriteDims.x * spriteDims.y - 1);
 
@@ -37,10 +37,10 @@ vec4 sampleVolumeAtlasInterpolated(in sampler2D volume, in ivec2 spriteDims, in 
     float tileSizeX = 1.0 / float(spriteDims.x);
     float tileSizeY = 1.0 / float(spriteDims.y);
 
-    float u  = (float(col)  + clampedPos.x) * tileSizeX;
-    float v  = (float(row)  + clampedPos.y) * tileSizeY;
-    float u2 = (float(col2) + clampedPos.x) * tileSizeX;
-    float v2 = (float(row2) + clampedPos.y) * tileSizeY;
+    float u  = (float(col)  + texCoord3D.x) * tileSizeX;
+    float v  = (float(row)  + texCoord3D.y) * tileSizeY;
+    float u2 = (float(col2) + texCoord3D.x) * tileSizeX;
+    float v2 = (float(row2) + texCoord3D.y) * tileSizeY;
 
     vec4 sample1 = texture(volume, vec2(u,  v));
     vec4 sample2 = texture(volume, vec2(u2, v2));
